7a68ea38e820b5252a7708fb0bd90e6fecf47ce7
Jonatan Männchen <jonatan@maennchen.ch>
2023-04-01 03:02:51 +0200

    Release v0.1.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@
 defmodule Purl.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/jshmrtn/purl"
   @description "Implementation of the purl (package url) specification"
 
