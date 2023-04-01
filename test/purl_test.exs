b56caac92e7e1fdc1c1ee2cc354f5935e49387f0
Jonatan Männchen <jonatan@maennchen.ch>
2023-04-01 02:49:59 +0200

    Purl from known resurce URI

--- test/purl_test.exs
+++ test/purl_test.exs
@@ -28,6 +28,37 @@ defmodule PurlTest do
     end
   end
 
+  describe inspect(&Purl.from_resource_uri/1) do
+    test "GitHub git url" do
+      assert {:ok, %Purl{type: "github", namespace: ["jshmrtn"], name: "purl"}} =
+               Purl.from_resource_uri("git@github.com:jshmrtn/purl.git")
+    end
+
+    test "GitHub http url" do
+      assert {:ok, %Purl{type: "github", namespace: ["jshmrtn"], name: "purl"}} =
+               Purl.from_resource_uri("https://github.com/jshmrtn/purl.git")
+    end
+
+    test "BitBucket git url" do
+      assert {:ok, %Purl{type: "bitbucket", namespace: ["jshmrtn"], name: "purl"}} =
+               Purl.from_resource_uri("git@bitbucket.org:jshmrtn/purl.git")
+    end
+
+    test "BitBucket http url" do
+      assert {:ok, %Purl{type: "bitbucket", namespace: ["jshmrtn"], name: "purl"}} =
+               Purl.from_resource_uri("https://irrelevant@bitbucket.org/jshmrtn/purl.git")
+    end
+
+    test "Hex.pm http url" do
+      assert {:ok, %Purl{type: "hex", namespace: [], name: "expo"}} =
+               Purl.from_resource_uri("https://hex.pm/packages/expo")
+    end
+
+    test "Random URL" do
+      assert :error = Purl.from_resource_uri("https://example.com")
+    end
+  end
+
   describe "specification verification" do
     for %{"description" => description, "is_invalid" => is_invalid?, "purl" => purl} =
           verification <- spec_tests do
